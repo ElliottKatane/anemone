dad19b6753f13ac9ed2457d12b35a9d50ea81d66
Elliott Katane <elliott.katane@gmail.com>
2023-11-17 11:51:28 -0500

    update server vercel et package json

--- backend/server.js
+++ backend/server.js
@@ -4,49 +4,17 @@ const mongoose = require("mongoose");
 const path = require("path");
 // routes import
 // Importez vos routes
-const userRoutes = require(path.join(__dirname, "routes", "userRoutes"));
-const verbRoutes = require(path.join(__dirname, "routes", "verbRoutes"));
+const userRoutes = require(path.join(__dirname, "routes", "userRoute"));
+const verbRoutes = require(path.join(__dirname, "routes", "verbRoute"));
 //express app
 const app = express();
 
 // middleware
 app.use(express.json());
+
 require("dotenv").config({ path: "./config.env" });
-app.use(
-  cors({
-    origin: ["https://anemone-alpha.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
-
-//allow CORS https://vercel.com/guides/how-to-enable-cors?query=cors#understanding-cors
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-module.exports = allowCors(handler);
+// require("dotenv").config();
+app.use(cors());
 
 // ejs
 app.set("view engine", "ejs"); // Set EJS as the view engine
@@ -67,12 +35,7 @@ app.get("/test", (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
